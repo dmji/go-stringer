d670e7f375433afdaa26c830a81b3dbbc9e82575
daimonji <leyshkinanton@gmail.com>
2024-12-31 13:57:40 +0300

    update and regenerate text transform enum

--- text_transform_stringy.go
+++ text_transform_stringy.go
@@ -10,15 +10,24 @@ import (
 	"github.com/gobeam/stringy"
 )
 
-//go:generate go run . -type=_TextConvertTo -nametransform=snake_case -trimprefix=_TextConvertTo -output=text_transform_stringy_string.go -genfromstringfn=true
+//go:generate go run . -type=_TextConvertTo -nametransform=snake_case_lower -trimprefix=_TextConvertTo -output=text_transform_stringy_string.go -genfromstringfn=true
 
 type _TextConvertTo int
 
 const (
 	_TextConvertToNone _TextConvertTo = iota
+
 	_TextConvertToCamelCase
 	_TextConvertToSnakeCase
 	_TextConvertToKebabCase
+
+	_TextConvertToCamelCaseLower
+	_TextConvertToSnakeCaseLower
+	_TextConvertToKebabCaseLower
+
+	_TextConvertToCamelCaseUpper
+	_TextConvertToSnakeCaseUpper
+	_TextConvertToKebabCaseUpper
 
 	_TextConvertToCount
 )
@@ -27,11 +36,26 @@ func transformTextTo(nametransform _TextConvertTo, name string) string {
 
 	switch nametransform {
 	case _TextConvertToCamelCase:
-		name = stringy.New(name).CamelCase().ToLower()
+		name = stringy.New(name).CamelCase().Get()
 	case _TextConvertToKebabCase:
-		name = stringy.New(name).KebabCase().ToLower()
+		name = stringy.New(name).KebabCase().Get()
 	case _TextConvertToSnakeCase:
+		name = stringy.New(name).SnakeCase().Get()
+
+	case _TextConvertToCamelCaseLower:
+		name = stringy.New(name).CamelCase().ToLower()
+	case _TextConvertToKebabCaseLower:
+		name = stringy.New(name).KebabCase().ToLower()
+	case _TextConvertToSnakeCaseLower:
 		name = stringy.New(name).SnakeCase().ToLower()
+
+	case _TextConvertToCamelCaseUpper:
+		name = stringy.New(name).CamelCase().ToUpper()
+	case _TextConvertToKebabCaseUpper:
+		name = stringy.New(name).KebabCase().ToUpper()
+	case _TextConvertToSnakeCaseUpper:
+		name = stringy.New(name).SnakeCase().ToUpper()
+
 	case _TextConvertToNone:
 	default:
 		panic(fmt.Sprintf("unexpected _TextConvertTo: %#v", nametransform))
